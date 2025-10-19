8521cb038e066c75bb75c1ba2e02c1a05e29062e
kysee <kysee.is@gmail.com>
2025-10-19 16:18:32 +0900

    test: Add test for fake merkle

--- zk-asset/transfer_test.go
+++ zk-asset/transfer_test.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/consensys/gnark/backend/plonk"
+	"github.com/consensys/gnark/constraint"
 	"github.com/holiman/uint256"
 	"github.com/kysee/zkp/zk-asset/node"
 	"github.com/kysee/zkp/zk-asset/types"
@@ -12,6 +14,8 @@ import (
 
 var (
 	wallets []*Wallet
+	css     constraint.ConstraintSystem
+	prKey   plonk.ProvingKey
 )
 
 func init() {
@@ -39,15 +43,34 @@ func init() {
 		}
 		wallets[i].AddSecretNote(secretNote)
 	}
+
+	css, prKey, _ = types.CompileCircuit(node.GetNoteCommitmentMerkleDepth())
 }
 
 func TestTransfer(t *testing.T) {
-	css, prKey, _ := types.CompileCircuit(node.GetNoteCommitmentMerkleDepth())
-
 	sender := wallets[0]
 	receiver := wallets[5]
+	amt, fee := uint256.NewInt(10), uint256.NewInt(0)
 
-	zkTx, err := sender.TransferProof(receiver.Address, uint256.NewInt(10), uint256.NewInt(0), prKey, css)
+	senderBalance0 := sender.GetBalance()
+	recieverBalance0 := receiver.GetBalance()
+
+	useSecretNote := sender.GetSecretNote(0)
+	useNote := useSecretNote.ToNoteOf(sender.PrivateKey.Public())
+	useNoteCommitment := useNote.Commitment()
+
+	// get merkle proof info.
+	rootHash, proofPath, depth, idx, _, err := node.GetNoteCommitmentMerkle(useNoteCommitment)
+	require.NoError(t, err)
+	//fmt.Printf("Merkle Info: numLeaves=%d, idx=%d, depth=%d, proofPath.len=%d\n", numLeaves, idx, depth, len(proofPath))
+
+	// generate the ZKTx including zk-proof
+	zkTx, newNotes, err := sender.TransferProof(
+		receiver.Address, amt, fee,
+		useNote,
+		rootHash, proofPath, depth, idx,
+		prKey, css,
+	)
 	require.NoError(t, err)
 	fmt.Printf("proof      : (%4dB) %x\n", len(zkTx.ProofBytes), zkTx.ProofBytes)
 	fmt.Printf("merkle root: (%4dB) %x\n", len(zkTx.MerkleRoot), zkTx.MerkleRoot)
@@ -55,9 +78,88 @@ func TestTransfer(t *testing.T) {
 	fmt.Printf("newNote    : (%4dB) %x\n", len(zkTx.NewNoteCommitment), zkTx.NewNoteCommitment)
 	fmt.Printf("changeNote : (%4dB) %x\n", len(zkTx.ChangeNoteCommitment), zkTx.ChangeNoteCommitment)
 
-	require.NoError(t, node.SendZKTransaction(zkTx))
+	// send the ZKTx to the node
+	err = node.SendZKTransaction(zkTx)
+	require.NoError(t, err)
 
-	newRoot := node.GetNoteCommitmentsRoot()
-	fmt.Printf("---\nnew merkle : (%4dB) %x\n", len(newRoot), newRoot)
+	fmt.Println("---")
 
+	sender.DelSecretNote(useSecretNote)
+	sender.AddSecretNote(newNotes[1].ToSecretNote()) // for change
+	receiver.AddSecretNote(newNotes[0].ToSecretNote())
+
+	senderBalance1 := sender.GetBalance()
+	recieverBalance1 := receiver.GetBalance()
+	require.EqualValues(t, new(uint256.Int).Sub(senderBalance0, new(uint256.Int).Add(amt, fee)), senderBalance1)
+	require.EqualValues(t, new(uint256.Int).Add(recieverBalance0, amt), recieverBalance1)
+
+	fmt.Println("sender balance  : ", senderBalance0.Dec(), ">", senderBalance1.Dec())
+	fmt.Println("receiver balance: ", recieverBalance0.Dec(), ">", recieverBalance1.Dec())
+}
+
+func TestTransfer_WrongMerkleRootHash(t *testing.T) {
+	sender := wallets[0]
+	receiver := wallets[5]
+	amt, fee := uint256.NewInt(10), uint256.NewInt(0)
+
+	useSecretNote := sender.GetSecretNote(0)
+	useNote := useSecretNote.ToNoteOf(sender.PrivateKey.Public())
+	useNoteCommitment := useNote.Commitment()
+
+	// get merkle proof info.
+	rootHash, proofPath, depth, idx, _, err := node.GetNoteCommitmentMerkle(useNoteCommitment)
+	require.NoError(t, err)
+	//fmt.Printf("Merkle Info: numLeaves=%d, idx=%d, depth=%d, proofPath.len=%d\n", numLeaves, idx, depth, len(proofPath))
+
+	// modifies the rootHash
+	rootHash[0], rootHash[1] = rootHash[1], rootHash[0]
+
+	// generate the ZKTx including zk-proof
+	_, _, err = sender.TransferProof(
+		receiver.Address, amt, fee,
+		useNote,
+		rootHash, proofPath, depth, idx,
+		prKey, css,
+	)
+	require.Error(t, err)
+}
+
+func TestTransfer_NonExistNote(t *testing.T) {
+	sender := wallets[0]
+	receiver := wallets[5]
+	amt, fee := uint256.NewInt(10), uint256.NewInt(0)
+
+	nonExistNote := &types.Note{
+		Version: 1,
+		PubKey:  sender.PrivateKey.Public(),
+		Balance: uint256.NewInt(1_000_000),
+		Salt:    types.RandBytes(32),
+	}
+
+	// get merkle proof info for the existing note.
+	existNote := sender.GetSecretNote(0).ToNoteOf(sender.PrivateKey.Public())
+	rootHash, proofPath, depth, idx, numLeaves, err := node.GetNoteCommitmentMerkle(existNote.Commitment())
+	require.NoError(t, err)
+	fmt.Printf("Merkle Info: numLeaves=%d, idx=%d, depth=%d, proofPath.len=%d\n", numLeaves, idx, depth, len(proofPath))
+
+	// expected error: nonExistNote.Commitment() is not in the proofPath
+	_, _, err = sender.TransferProof(
+		receiver.Address, amt, fee,
+		nonExistNote,
+		rootHash, proofPath, depth, idx,
+		prKey, css,
+	)
+	require.Error(t, err)
+
+	// make the proofPath have the nonExistNote.Commitment()
+	proofPath[0] = nonExistNote.Commitment()
+
+	// expected error: rootHash is not same
+	_, _, err = sender.TransferProof(
+		receiver.Address, amt, fee,
+		nonExistNote,
+		rootHash, proofPath, depth, idx,
+		prKey, css,
+	)
+	require.Error(t, err)
 }
